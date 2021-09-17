4024ef4a1bc03a7b8b99167c146b7c0df7398921
ping58972 <ping58972@gmail.com>
2021-09-17 16:48:01 -0500

    deploy to github page

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,18 @@ const sync = require('./build/browsersync');
 
 gulp.task('build', gulp.series(['sass', 'scripts', 'images', 'jekyll-build']));
 
-var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
+// var gulp = require('gulp');
+// var deploy = require('gulp-gh-pages');
 
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function() {
-    return gulp.src("./dist/**/*")
-        .pipe(deploy())
-});
+// /**
+//  * Push build to gh-pages
+//  */
+// gulp.task('deploy', function() {
+//     return gulp.src("./dist/**/*")
+//         .pipe(deploy())
+// });
+
+const { src, task } = require('gulp');
+const ghPages = require('gulp-gh-pages');
+
+task('deploy', () => src('./').pipe(ghPages()));
