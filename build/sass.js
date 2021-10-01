a6510188fdb2f1d43bfef45663c651552ea00013
ping58972 <ping58972@gmail.com>
2021-10-01 14:33:07 -0500

    fix for deploy

--- build/sass.js
+++ build/sass.js
@@ -1,4 +1,4 @@
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass')); //require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
@@ -6,23 +6,23 @@ const scssPath = '_scss/*.scss';
 const destPath = '_site/css';
 
 module.exports = gulp => {
-  gulp.task('sass', () => {
-    return gulp
-      .src(scssPath)
-      .pipe(
-        sass({
-          includePaths: ['scss'],
-          outputStyle: 'expanded',
-        })
-      )
-      .pipe(
-        prefix({
-          overrideBrowserslist: ['last 2 versions'],
-          cascade: false,
-        })
-      )
-      .pipe(cleanCSS({ compatibility: 'ie8' }))
-      .pipe(gulp.dest(destPath))
-      .pipe(gulp.dest('css'));
-  });
+    gulp.task('sass', () => {
+        return gulp
+            .src(scssPath)
+            .pipe(
+                sass({
+                    includePaths: ['scss'],
+                    outputStyle: 'expanded',
+                })
+            )
+            .pipe(
+                prefix({
+                    overrideBrowserslist: ['last 2 versions'],
+                    cascade: false,
+                })
+            )
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
+            .pipe(gulp.dest(destPath))
+            .pipe(gulp.dest('css'));
+    });
 };
